d058e8a08dda7887aa986548ada9cf109fe588a8
Ewout Prangsma <ewout@prangsma.net>
2021-07-08 10:06:50 +0200

    Trying doc.go in parent folder

--- metrics/doc.go
+++ metrics/doc.go
@@ -23,4 +23,4 @@
 // Package metrics/v1 contains the API of Metrics services.
 package v1
 
-//go:generate protoc -I .:../../:../../vendor/:../../vendor/googleapis/:../../vendor/github.com/gogo/protobuf/protobuf/ --gofast_out=Mgithub.com/golang/protobuf/ptypes/duration/duration.proto=github.com/gogo/protobuf/types,Mgithub.com/golang/protobuf/ptypes/timestamp/timestamp.proto=github.com/gogo/protobuf/types,plugins=grpc,paths=source_relative:. --grpc-gateway_out=paths=source_relative,logtostderr=true,allow_delete_body=true:. ./oasis_metrics.proto
+//go:generate protoc -I v1:../:../vendor/:../vendor/googleapis/:../vendor/github.com/gogo/protobuf/protobuf/ --gofast_out=Mgithub.com/golang/protobuf/ptypes/duration/duration.proto=github.com/gogo/protobuf/types,Mgithub.com/golang/protobuf/ptypes/timestamp/timestamp.proto=github.com/gogo/protobuf/types,plugins=grpc,paths=source_relative:v1 --grpc-gateway_out=paths=source_relative,logtostderr=true,allow_delete_body=true:v1 ./v1/metrics.proto
