3de20d01ef38984d929c6b8d84aa1226041ccc19
jwierzbo <jakub.wierzbowski@arangodb.com>
2024-10-23 23:36:46 +0200

    OAS-10239 Protobuf update

--- common/v1/doc.go
+++ common/v1/doc.go
@@ -23,4 +23,4 @@
 // Package common/v1 contains the common types used in API of ArangoDB Managed Service
 package v1
 
-//go:generate protoc -I .:../../:../../vendor/ --gofast_out=Mgithub.com/golang/protobuf/ptypes/timestamp/timestamp.proto=github.com/gogo/protobuf/types,plugins=grpc,paths=source_relative:. ./common.proto
+//go:generate protoc -I .:../../:../../vendor/ --gofast_out=Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,plugins=grpc,paths=source_relative:. ./common.proto
